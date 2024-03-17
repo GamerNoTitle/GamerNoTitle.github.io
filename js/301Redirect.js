02f97a1fbc206f4a86088f6a1bbaba2bc1f94539
GamerNoTitle <GamerNoTitle@outlook.com>
2024-03-17 03:42:54 +0000

    Deploy with Github Action

---
+++ js/301Redirect.js
@@ -0,0 +1,6 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/assets/js/Meting.min.js"></script>url = window.location.href
+whether = String(url).search(/20[0-2][0-9]\/[0-1][1-9]\/[0-3][1-9]/i)
+if(whether == True){
+    result = String(url).replace(/20[0-2][0-9]\/[0-1][1-9]\/[0-3][1-9]/i,'posts')
+    window.location = result
+}
